a5b67ad21b55a8123cf61a3cf8d16f8a497211b0
Ki Hyun Kim <pointzz.ki@gmail.com>
2018-04-22 10:05:51 +0900

    correct link from soft to hard

--- detokenizer.py
+++ detokenizer.py
@@ -1,1 +1,8 @@
-/home/khkim/Workspace/nlp/corpus/parallel_corpora/detokenizer.py
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        if line.strip() != "":
+            line = line.strip().replace(' ', '').replace('▁▁', ' ').replace('▁', '').strip()
+
+            sys.stdout.write(line + '\n')
